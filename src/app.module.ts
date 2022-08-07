d02a649c05738b36e44f1b271d5c79cbbdcd3c4b
Jan <jan.blaszczyk@protonmail.com>
2022-08-07 18:50:18 +0200

    Basics modules generated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { BookModule } from './book/book.module';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, BookModule, AdminModule],
   controllers: [AppController],
   providers: [AppService],
 })
