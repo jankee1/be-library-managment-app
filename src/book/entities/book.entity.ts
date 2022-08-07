9c6faf5fd55fa98543cdd71eee36bde5796af717
Jan <jan.blaszczyk@protonmail.com>
2022-08-07 19:25:55 +0200

    book entity along with dto created

--- src/book/entities/book.entity.ts
+++ src/book/entities/book.entity.ts
@@ -1,1 +1,19 @@
-export class Book {}
+import { USER_INPUT_BOOK_TITLE_MAX_LENGTH, USER_INPUT_BOOK_AUTHOR_MAX_LENGTH } from './../../consts';
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class BookEntity {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({length: USER_INPUT_BOOK_TITLE_MAX_LENGTH})
+    title: string;
+
+    @Column({length: USER_INPUT_BOOK_AUTHOR_MAX_LENGTH})
+    author: string;
+
+    @Column()
+    publishedOn: Date;
+    
+}
