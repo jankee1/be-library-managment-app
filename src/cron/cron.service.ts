6cec4aac4a79e57eec5b9802075782add23adcfb
Jan <jan.blaszczyk@protonmail.com>
2022-08-16 20:48:34 +0200

    Implementation of feesadder completed.Fees are added every day at 00:01 AM

--- src/cron/cron.service.ts
+++ src/cron/cron.service.ts
@@ -1,26 +1,25 @@
 import { BorrowedBookEntity } from './../borrowed-book/entities/borrowed-book.entity';
 import { Injectable } from '@nestjs/common';
-import { Cron, CronExpression } from '@nestjs/schedule';
-import { DataSource } from 'typeorm';
+import { Cron } from '@nestjs/schedule';
+import { LessThan } from 'typeorm';
 
 @Injectable()
 export class CronService {
 
+    private additionalFees: number = 1
     private daysBack: number = 30;
-    private daysBackForFees = new Date(new Date().setDate(new Date().getDate() - this.daysBack))
+    private daysBackForFees: Date = new Date(new Date().setDate(new Date().getDate() - this.daysBack))
 
-    constructor(private dataSource: DataSource){}
-
-    @Cron(CronExpression.EVERY_5_SECONDS )
+    @Cron("1 0 * * *") // every day at 00:01
     async calculateFees() {
 
-        const borrowedBooks = await this.dataSource
-            .createQueryBuilder()
-            .select(["Id", "fees"])
-            .from(BorrowedBookEntity, "BorrowedBookEntity")
-            .where("borrowedAt < :daysBackForFees", {daysBackForFees: this.daysBackForFees})
-            .execute()
+        const borrowedBooks: BorrowedBookEntity[]  = await BorrowedBookEntity.find({
+            where: {borrowedAt: LessThan(this.daysBackForFees)},
+        })
 
-        console.log(borrowedBooks)
+        for(const book of borrowedBooks) {
+            book.fees = book.fees + this.additionalFees
+            await book.save();
+        }
     }
 }
